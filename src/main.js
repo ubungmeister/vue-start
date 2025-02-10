5dc9f77c2e58f54cbd5b88c19cb7341c5f092564
ubungmeister <ubungmeister@gmail.com>
2025-02-10 12:56:59 +0100

    initial

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
+/* eslint-disable prettier/prettier */
 import { createApp } from "vue";
 import App from "./App.vue";
-import './assets/tailwind.css'
-
+// import './assets/tailwind.css'
+import "./app.css";
 createApp(App).mount("#app");
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
